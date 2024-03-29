96e7f1f155f04a9e6bc6bac38a4e453a6b5e8384
Sibin George <sibingeorge009@gmail.com>
2024-03-29 13:02:29 -0400

    changes added for showing the appointment details after booking the appointment

--- controllers/getG2Page.js
+++ controllers/getG2Page.js
@@ -1,8 +1,18 @@
 const info = require('../models/InfoModel.js') // imports Info model
+const appointment = require('../models/AppointmentModel'); // imports Appointment model
 module.exports = async (req, res) => {
-    if (req.session.userId) { 
+    if (req.session.userId) {
         data = await info.findOne({ _id: req.session.userId });
-        res.render('g2', { data });
+        if (data.appointmentId) {
+            const userAppointment = await appointment.findOne({ _id: data.appointmentId });
+            const userDetails = {
+                ...data.toObject(),
+                appointment: userAppointment
+            };
+            res.render('g2', { data: userDetails });
+        } else {
+            res.render('g2', { data });
+        }
     } else {
         res.render('g2');
     }
--- public/js/appointmentSelection.js
+++ public/js/appointmentSelection.js
@@ -1,13 +1,11 @@
-appt = {
-    appointmentDate: '',
-    appointmentTime: ''
-};
-
 document.addEventListener('DOMContentLoaded', function () {
-    updateAppointmentTimes();
+    updateAppointmentTimes(); // populate time options based on current date
 });
 
-// Function to get current date in YYYY-MM-DD format
+// add event listener to update time options when date is changed
+document.getElementById('appointmentDate').addEventListener('change', updateAppointmentTimes);
+
+// function to get current date in YYYY-MM-DD format
 function getCurrentDate(date) {
     const currentDate = date ? new Date(date) : new Date();
     const year = currentDate.getFullYear();
@@ -16,60 +14,36 @@ function getCurrentDate(date) {
     return `${year}-${month}-${day}`;
 }
 
-async function bookAppointment(appointment, time) {
-
-    appt = {
-        appointmentDate: getCurrentDate(appointment.date),
-        appointmentTime: time
-    };
+// function to book an appointment slot
+function bookAppointment(appointment) {
 
     const user = {
         ...data,
         appointmentId: appointment._id
     }
 
-    // fetch('/bookAppointment', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(user),
-    // })
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error('Network response was not ok');
-    //         }
-    //         // window.location.reload();
-    //         // return response.json();
-    //     })
-    //     .then(userDetails => {
-    //         console.log('User details:', userDetails);
-    //         // window.location.reload();
-    //     })
-    //     .catch(error => {
-    //         console.error('Error booking appointment:', error);
-    //     });
-
-    try {
-        const response = await fetch('/bookAppointment', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
+    fetch('/bookAppointment', { // fetch request to book appointment
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(userDetails => {
+            window.location.reload(); // reload the page after booking appointment
+        })
+        .catch(error => {
+            console.error('Error booking appointment:', error);
         });
-
-        if (!response.ok) {
-            throw new Error('Failed to book appointments');
-        }
-        const appts = await response.json();
-        console.log('Appointments:', appts);
-    } catch (error) {
-        console.error('Error booking appointments:', error);
-        throw error; // Rethrow the error to propagate it to the caller
-    }
 }
 
+// function to fetch appointments based on selected date
 async function fetchAppointmentsByDate(selectedDate) {
     try {
         const response = await fetch(`/getAppointmentsByDate?date=${selectedDate}`);
@@ -83,12 +57,11 @@ async function fetchAppointmentsByDate(selectedDate) {
     }
 }
 
-// Function to populate time options based on selected date
+// function to populate time options based on selected date
 async function updateAppointmentTimes() {
     const selectedDate = document.getElementById('appointmentDate').value;
     try {
         const appointments = await fetchAppointmentsByDate(selectedDate);
-        const test = await fetchAppointmentsByDate(selectedDate);
 
         const appointmentTimes = appointments
             .filter(appointment => appointment.isTimeSlotAvailable)
@@ -97,18 +70,21 @@ async function updateAppointmentTimes() {
         const timeButtonContainer = document.getElementById('appointmentTimeButtons');
         timeButtonContainer.innerHTML = '';
 
+        // if there are no available times for the selected date, display a message
         if (appointmentTimes.length === 0) {
             const noTimesMessage = document.createElement('p');
             noTimesMessage.textContent = 'No available times for selected date';
             timeButtonContainer.appendChild(noTimesMessage);
         } else {
+            // create a button for each available time slot and add an event listener to book the appointment
             appointmentTimes.forEach(time => {
                 const timeButton = document.createElement('button');
                 timeButton.classList.add('btn', 'btn-success', 'mx-1', 'my-1');
                 timeButton.textContent = time;
                 const appointment = appointments.find(x => x.time === time);
-                timeButton.addEventListener('click', async () => {
-                    await bookAppointment(appointment, time);
+                timeButton.addEventListener('click', event => {
+                    event.preventDefault();
+                    bookAppointment(appointment);
                 });
                 timeButtonContainer.appendChild(timeButton);
             });
@@ -117,5 +93,3 @@ async function updateAppointmentTimes() {
         console.error('Error fetching appointments:', error);
     }
 }
-
-document.getElementById('appointmentDate').addEventListener('change', updateAppointmentTimes);
